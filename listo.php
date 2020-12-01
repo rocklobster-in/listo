6c9f852d61c1398dfd9fdf105f639ea36aca89f7
Yordan Soares <contacto@yordansoar.es>
2020-12-01 03:08:44 -0400

    Add subdivisions of Argentina
    The list of subdivisions of Argentina based on ISO 3166-2:AR standard.
    
    Source: https://en.wikipedia.org/wiki/ISO_3166-2:AR ISO 3166-2:AR

--- listo.php
+++ listo.php
@@ -29,6 +29,7 @@ class Listo_Manager {
 	private static function get_list_types() {
 		$list_types = array(
 			'countries' => 'Listo_Countries',
+			'ar_subdivisions' => 'Listo_AR_Subdivisions',
 			'in_subdivisions' => 'Listo_IN_Subdivisions',
 			'us_subdivisions' => 'Listo_US_Subdivisions',
 			've_subdivisions' => 'Listo_VE_Subdivisions',
