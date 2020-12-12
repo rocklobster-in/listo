57cb0d03ea32ba4dac50ebc634e3b56eadf30c40
Yordan Soares <contacto@yordansoar.es>
2020-12-11 23:28:02 -0400

    Add subdivisions of Costa Rica
    The list of subdivisions of Costa Rica based on ISO 3166-2:CR standard.
    
    Source: https://en.wikipedia.org/wiki/ISO_3166-2:CR ISO 3166-2:CR

---
+++ modules/cr-subdivisions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The list of subdivisions of Costa Rica based on ISO 3166-2:CR standard.
+ *
+ * Source: https://en.wikipedia.org/wiki/ISO_3166-2:CR ISO 3166-2:CR
+ */
+class Listo_CR_Subdivisions implements Listo {
+	private function __construct() {}
+
+	public static function items() {
+		return array(
+			'a' => _x( 'Alajuela', 'cr_subdivisions', 'listo' ),
+			'c' => _x( 'Cartago', 'cr_subdivisions', 'listo' ),
+			'g' => _x( 'Guanacaste', 'cr_subdivisions', 'listo' ),
+			'h' => _x( 'Heredia', 'cr_subdivisions', 'listo' ),
+			'l' => _x( 'Limón', 'cr_subdivisions', 'listo' ),
+			'p' => _x( 'Puntarenas', 'cr_subdivisions', 'listo' ),
+			'sj' => _x( 'San José', 'cr_subdivisions', 'listo' ),
+		);
+	}
+
+	public static function groups() {
+		return array();
+	}
+}
