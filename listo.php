aee789fa2173a73c727e34f8fa9498433cfe0045
Yordan Soares <contacto@yordansoar.es>
2021-02-08 07:08:57 -0400

    Add subdivisions of Paraguay
    The list of subdivisions of Paraguay based on ISO 3166-2:PY standard.
    
    Source: https://en.wikipedia.org/wiki/ISO_3166-2:PY ISO 3166-2:PY

--- listo.php
+++ listo.php
@@ -44,6 +44,7 @@ class Listo_Manager {
 			'mx_subdivisions' => 'Listo_MX_Subdivisions',
 			'ni_subdivisions' => 'Listo_NI_Subdivisions',
 			'pa_subdivisions' => 'Listo_PA_Subdivisions',
+			'py_subdivisions' => 'Listo_PY_Subdivisions',
 			'sv_subdivisions' => 'Listo_SV_Subdivisions',
 			'us_subdivisions' => 'Listo_US_Subdivisions',
 			've_subdivisions' => 'Listo_VE_Subdivisions',
