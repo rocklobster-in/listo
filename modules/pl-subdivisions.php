7c6b599a1fd236ff3614ff021adce416bfbd3442
Hubert Hart <71757715+hubi-moore@users.noreply.github.com>
2022-03-15 08:54:34 +0100

    Update pl-subdivisions.php

--- modules/pl-subdivisions.php
+++ modules/pl-subdivisions.php
@@ -9,29 +9,29 @@ class Listo_PL_Subdivisions implements Listo {
 
 	public static function items() {
 		return array(
-			'd' => _x( 'dolnośląskie', 'pl_subdivisions', 'listo' ),
-			'c' => _x( 'kujawsko-pomorskie', 'pl_subdivisions', 'listo' ),
-			'l' => _x( 'lubelskie', 'pl_subdivisions', 'listo' ),
-			'f' => _x( 'lubuskie', 'pl_subdivisions', 'listo' ),
-			'e' => _x( 'łódzkie', 'pl_subdivisions', 'listo' ),
-			'k' => _x( 'małopolskie', 'pl_subdivisions', 'listo' ),
-			'w' => _x( 'mazowieckie', 'pl_subdivisions', 'listo' ),
-			'o' => _x( 'opolskie', 'pl_subdivisions', 'listo' ),
-			'r' => _x( 'podkarpackie', 'pl_subdivisions', 'listo' ),
-			'b' => _x( 'podlaskie', 'pl_subdivisions', 'listo' ),
-			'g' => _x( 'pomorskie', 'pl_subdivisions', 'listo' ),
-			's' => _x( 'śląskie', 'pl_subdivisions', 'listo' ),
-			't' => _x( 'świętokrzyskie', 'pl_subdivisions', 'listo' ),
-			'n' => _x( 'warmińsko-mazurskie', 'pl_subdivisions', 'listo' ),
-			'p' => _x( 'wielkopolskie', 'pl_subdivisions', 'listo' ),
-			'z' => _x( 'zachodniopomorskie', 'pl_subdivisions', 'listo' ),
+			'02' => _x( 'Dolnośląskie', 'pl_subdivisions', 'listo' ),
+			'04' => _x( 'Kujawsko-pomorskie', 'pl_subdivisions', 'listo' ),
+			'06' => _x( 'Lubelskie', 'pl_subdivisions', 'listo' ),
+			'08' => _x( 'Lubuskie', 'pl_subdivisions', 'listo' ),
+			'10' => _x( 'Łódzkie', 'pl_subdivisions', 'listo' ),
+			'12' => _x( 'Małopolskie', 'pl_subdivisions', 'listo' ),
+			'14' => _x( 'Mazowieckie', 'pl_subdivisions', 'listo' ),
+			'16' => _x( 'Opolskie', 'pl_subdivisions', 'listo' ),
+			'18' => _x( 'Podkarpackie', 'pl_subdivisions', 'listo' ),
+			'20' => _x( 'Podlaskie', 'pl_subdivisions', 'listo' ),
+			'22' => _x( 'Pomorskie', 'pl_subdivisions', 'listo' ),
+			'24' => _x( 'Śląskie', 'pl_subdivisions', 'listo' ),
+			'26' => _x( 'Świętokrzyskie', 'pl_subdivisions', 'listo' ),
+			'28' => _x( 'Warmińsko-mazurskie', 'pl_subdivisions', 'listo' ),
+			'30' => _x( 'Wielkopolskie', 'pl_subdivisions', 'listo' ),
+			'32' => _x( 'Zachodniopomorskie', 'pl_subdivisions', 'listo' ),
 		);
 	}
 
 	public static function groups() {
 		return array(
-			'voivodeships' => array( 'd', 'c', 'l', 'f', 'e', 'k', 'w', 'o',
-				'r', 'b', 'g', 's', 't', 'n', 'p', 'z',
+			'voivodeships' => array( '02', '04', '06', '08', '10', '12', '14', '16',
+				'18', '20', '22', '24', '26', '28', '30', '32',
 			),
 		);
 	}
